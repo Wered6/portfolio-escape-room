6d998a834718e403f9f114f94546892e7eae8594
Arek <arek0006@gmail.com>
2025-04-10 18:47:32 +0200

    Refactor UVALID macro

--- Source/EscapeRoom/Utility/WeredMacros.h
+++ Source/EscapeRoom/Utility/WeredMacros.h
@@ -8,21 +8,21 @@
  * Checks the validity of a UObject pointer, logs a warning if it is invalid to help prevent editor crashes, and then returns.
  * This has no effect in shipping builds.
  */
-#define UVALID_LOG_DEBUG(PointerParam)																						\
-{																															\
-	if (!IsValid(PointerParam))																									\
-	{																														\
-		UE_LOG(LogTemp, Warning, TEXT("%s|%s is nullptr#L%d"), TEXT(__FUNCTION__), TEXT(#PointerParam), __LINE__)		    \
-		return;																												\
-	}																														\
+#define UVALID_LOG_DEBUG(PointerParam)																					\
+{																														\
+	if (!IsValid(PointerParam))																							\
+	{																													\
+		UE_LOG(LogTemp, Warning, TEXT("%s is nullptr at \"%s#L%d\""), TEXT(#PointerParam),  TEXT(__FILE__), __LINE__)	\
+		return;																											\
+	}																													\
 }
-#define UVALID_LOG_DEBUGF(PointerParam)																						\
-{																															\
-	if (!IsValid(PointerParam))																									\
-	{																														\
-		UE_LOG(LogTemp, Warning, TEXT("%s|%s is nullptr#L%d"), TEXT(__FUNCTION__), TEXT(#PointerParam), __LINE__)		    \
-		return 0.f;																												\
-	}																														\
+#define UVALID_LOG_DEBUGF(PointerParam)																					\
+{																														\
+	if (!IsValid(PointerParam))																							\
+	{																													\
+		UE_LOG(LogTemp, Warning, TEXT("%s is nullptr at \"%s#L%d\""), TEXT(#PointerParam),  TEXT(__FILE__), __LINE__)	\
+		return 0.f;																										\
+	}																													\
 }
 #else
 #define UVALID_LOG_DEBUG(PointerParam)  /* No-op in shipping builds */
